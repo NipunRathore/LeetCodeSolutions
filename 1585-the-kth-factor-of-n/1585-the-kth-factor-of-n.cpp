6a629bbfa23026adf105271db777f965e9ace013
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2023-11-01 16:08:53 +0530

    Time: 0 ms (100%), Space: 6.3 MB (62.91%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int> factor ; 
+        int res = n ; 
+        int cnt = 0 ; 
         for (int i = 1 ; i <= n ; i ++)
         {
             if (n % i == 0)
             {
-                factor.push_back(i) ; 
+                k -- ; 
+            }
+
+            if (k == 0)
+            {
+                return i ; 
             }
         }
-        if (factor.size() < k)
-        {
-            return -1 ; 
-        }
-        return factor[k - 1] ; 
+        return -1 ;
     }
 };
+
+// class Solution {
+// public:
+//     int kthFactor(int n, int k) {
+//         vector<int> factor ; 
+//         for (int i = 1 ; i <= n ; i ++)
+//         {
+//             if (n % i == 0)
+//             {
+//                 factor.push_back(i) ; 
+//             }
+//         }
+//         if (factor.size() < k)
+//         {
+//             return -1 ; 
+//         }
+//         return factor[k - 1] ; 
+//     }
+// };
+// S.C. = O(n)
+// T.C. = O(n)
