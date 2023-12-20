4eb2f24e57a44aa0303de8f2303245c4723baddc
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2023-12-20 21:56:34 +0530

    Time: 14 ms (48.64%), Space: 47 MB (58.30%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,13 +1,42 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end()) ;
-        if (prices[0] + prices[1] <= money)
+        int mini1 = INT_MAX ;
+        int mini2 = INT_MAX ; 
+
+        for (int i = 0 ; i < prices.size() ; i ++)
         {
-            return money - (prices[0] + prices[1]) ;
+            if (prices[i] <= mini1)
+            {
+                mini2 = mini1 ; 
+                mini1 = prices[i] ;
+            }
+            else if (prices[i] <= mini2 && prices[i] > mini1)
+            {
+                mini2 = prices[i] ;
+            }
+        }
+
+        if (mini1 + mini2 <= money)
+        {
+            return money - (mini1 + mini2) ;
         }
         return money ;
     }
 };
+// T.C. = O(N)
+// S.C. = O(1)
+
+// class Solution {
+// public:
+//     int buyChoco(vector<int>& prices, int money) {
+//         sort(prices.begin(), prices.end()) ;
+//         if (prices[0] + prices[1] <= money)
+//         {
+//             return money - (prices[0] + prices[1]) ;
+//         }
+//         return money ;
+//     }
+// };
 // T.C. = O(N * logN)
 // S.C. = O(1)
