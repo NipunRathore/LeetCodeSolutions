87bd0316554306390ac67befd5036515035a6fc3
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2023-12-30 16:04:08 +0530

    Time: 72 ms (9.90%), Space: 14.9 MB (50.82%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,28 +9,76 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (headA == NULL || headB == NULL)
+        if (headA == NULL || headB == NULL) 
         {
-            return NULL ; 
+            return NULL ;
         }
 
-        unordered_set<ListNode*> st ; 
-        ListNode* temp1 = headA ; 
-        while (temp1 != NULL)
-        {
-            st.insert(temp1) ; 
-            temp1 = temp1 -> next ;
-        }
+        ListNode* temp1 = headA ;
+        ListNode* temp2 = headB ;
 
-        ListNode* temp2 = headB ; 
-        while (temp2 != NULL)
+        while (temp1 != temp2) 
         {
-            if (st.find(temp2) != st.end())
+            // if temp1 is not NULL, move to the next node 
+            // else set temp1 to headB
+            if (temp1 != NULL)
             {
-                return temp2 ; 
+                temp1 = temp1 -> next ; 
             }
-            temp2 = temp2 -> next ; 
+            else 
+            {
+                temp1 = headB ; 
+            }
+            
+            // if temp2 is not NULL, move to the next node
+            // else set temp2 to headA
+            if (temp2 != NULL)
+            {
+                temp2 = temp2 -> next ;
+            }
+            else
+            {
+                temp2 = headA ; 
+            }
         }
-        return NULL ; 
+        
+        return temp1 ;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         if (headA == NULL || headB == NULL)
+//         {
+//             return NULL ; 
+//         }
+
+//         unordered_set<ListNode*> st ; 
+//         ListNode* temp1 = headA ; 
+//         while (temp1 != NULL)
+//         {
+//             st.insert(temp1) ; 
+//             temp1 = temp1 -> next ;
+//         }
+
+//         ListNode* temp2 = headB ; 
+//         while (temp2 != NULL)
+//         {
+//             if (st.find(temp2) != st.end())
+//             {
+//                 return temp2 ; 
+//             }
+//             temp2 = temp2 -> next ; 
+//         }
+//         return NULL ; 
+//     }
+// };
