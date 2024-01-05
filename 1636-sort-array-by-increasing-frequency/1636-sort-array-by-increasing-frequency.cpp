4d4f8bd12b3540444885b4efb2a3d0abbb0b1054
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-01-05 11:21:03 +0530

    Time: 10 ms (30.17%), Space: 11.7 MB (53.31%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -8,15 +8,15 @@ public:
             // if freq of two elements same we want to consider smaller element first therefore need to use negative sign 
             // 2 < 5
             // -2 > -5
-            mp[-nums[i]] ++ ;
+            mp[nums[i]] ++ ;
         }
-
-        // create a Min-Heap Priority Queue & insert {freq, element} pairs in it
-        // so that element at top of min freq 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq ;
+            
+        // create a Max-Heap Priority Queue & insert {freq, element} pairs in it
+        // so that element at top of max freq 
+        priority_queue<pair<int, int> > pq ;
         for (auto it : mp)
         {
-            pq.push({it.second, it.first}) ; 
+            pq.push({-it.second, it.first}) ; 
         }
         
         // fetch elements from pq & store in resultant array 
@@ -25,10 +25,10 @@ public:
         {
             int freq = pq.top().first ;
             int element = pq.top().second ;
-            for(int i = 0 ; i < freq ; i ++)
+            for(int i = 0 ; i < -1 * freq ; i ++)
             {
                 // using -ve sign to neutralise the -ve used earlier
-                ans.push_back(-element) ;
+                ans.push_back(element) ;
             }
             pq.pop() ;
         }
@@ -36,3 +36,42 @@ public:
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     vector<int> frequencySort(vector<int>& nums) {
+//         // store array elements in map 
+//         unordered_map<int, int> mp ;
+//         for(int i = 0 ; i < nums.size() ; i ++)
+//         {
+//             // if freq of two elements same we want to consider smaller element first therefore need to use negative sign 
+//             // 2 < 5
+//             // -2 > -5
+//             mp[-nums[i]] ++ ;
+//         }
+
+//         // create a Min-Heap Priority Queue & insert {freq, element} pairs in it
+//         // so that element at top of min freq 
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq ;
+//         for (auto it : mp)
+//         {
+//             pq.push({it.second, it.first}) ; 
+//         }
+        
+//         // fetch elements from pq & store in resultant array 
+//         vector<int> ans ;
+//         while(!pq.empty())
+//         {
+//             int freq = pq.top().first ;
+//             int element = pq.top().second ;
+//             for(int i = 0 ; i < freq ; i ++)
+//             {
+//                 // using -ve sign to neutralise the -ve used earlier
+//                 ans.push_back(-element) ;
+//             }
+//             pq.pop() ;
+//         }
+        
+//         return ans;
+//     }
+// };
