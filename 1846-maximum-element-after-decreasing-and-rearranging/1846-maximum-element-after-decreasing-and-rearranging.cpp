54def1639ef0bfd43dbec3b5f51c646517a17972
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-01-17 00:07:52 +0530

    Time: 75 ms (73.28%), Space: 51.8 MB (33.76%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        // sort array so elements arranged in a non decreasing fashion
         sort(arr.begin(), arr.end()) ; 
-        int curr = 0 ; 
-
-        for (int i = 0 ; i < arr.size() ; i ++)
+        
+        // make first element (0th index) equal to 1
+        arr[0] = 1 ; 
+        for (int i = 1 ; i < arr.size() ; i ++)
         {
-            if (arr[i] > curr)
+            // check if absolute difference between element & previous element greater than 1 or not
+            if (arr[i] > arr[i - 1] + 1)
             {
-                curr ++ ; 
+                // if yes, add one to previous element value & store at it 
+                // so that difference condition satisfied and we have maximised the element also 
+                arr[i] = arr[i - 1] + 1 ; 
             }
         }
-        return curr ; 
+        // return last element 
+        return arr.back() ; 
     }
 };
+// T.C. = O (N*logN)
+// S.C. = O (1)
+
+// class Solution {
+// public:
+//     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+//         sort(arr.begin(), arr.end()) ; 
+//         int curr = 0 ; 
+
+//         for (int i = 0 ; i < arr.size() ; i ++)
+//         {
+//             if (arr[i] > curr)
+//             {
+//                 curr ++ ; 
+//             }
+//         }
+//         return curr ; 
+//     }
+// };
