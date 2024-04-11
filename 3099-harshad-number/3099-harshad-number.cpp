b58ec094316b3b1a26274921b5afb4810e67b66c
Nipun Singh Rathore <83899944+NipunRathore@users.noreply.github.com>
2024-04-11 21:52:35 +0530

    Time: 0 ms (100.00%), Space: 7 MB (96.22%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int num = x ; 
+        int sum = 0 ; 
+
+        while (num != 0)
+        {
+            sum += num % 10 ; 
+            num /= 10 ;
+        }
+        if (x % sum == 0)
+        {
+            return sum ; 
+        }
+        return -1 ; 
+    }
+};
